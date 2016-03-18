60948c16655328fcd9262982da3648291acc4676
Gracie Lundell <gracielundell@gmail.com>
2016-03-18 11:55:19 -0700

    clear previously inputted info when new info added

--- build/js/app.js
+++ build/js/app.js
@@ -21,20 +21,19 @@ $(document).ready(function() {
   $("form#searchForm").submit(function(event) {
     var userName = $("#inputSearch").val();
     // get username
+    $("#inputSearch").empty();
+    $("#userImg").empty();
     $.get('https://api.github.com/users/' + userName +
      '/repos?access_token=' + apiKey).then(function(response){
-
-       console.log(response);
-
       $("#userName").append('<span>' + response[0].owner.login + '</span>');
       $("#userImg").append('<img src='+ response[0].owner.avatar_url+'>');
       // get userAvatar
       $.get('https://api.github.com/users/' + userName + '/followers?access_token=' + apiKey).then(function(response){
         // get userRepos
+        $(".postedRepos").empty();
         $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
           for (i = 0; i < response.length; i ++) {
-            $(".postedRepos").append("<ul><li>Repository Name: " + response[i].name + "Date Created: " + response[i].created_at + "</li></ul>");
-            console.log("repo name: " + response[i].name + " date created: " + response[i].created_at);
+            $(".postedRepos").append("<li><strong> Repository Name: </strong>" + response[i].name + "<br><strong>Date Created: </strong>" + response[i].created_at + "</li><br><br>");
           }; // end for-loop
         }); // end getRepos
       });
